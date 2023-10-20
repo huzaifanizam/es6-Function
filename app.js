73493208ce5289f484e0fe5ea68daacd22bac21b
huzaifanizam <nizamhuzaifa@gmail.com>
2023-10-20 22:19:39 +0500

    first commit

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-// function sum(num1,num2) {
-    
-//     let total = num1 + num2;
 
-//     return total
-// }
-window.prt = function() {
-    
-    console.log(new Date())
-}
-// prt();
-// prt();
-
-window.arg = function(arr) {
-    console.log("avg function in running");
-
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i];
-    }
-    let result = sum / arr.length;
-    result = result.toFixed(2);
-    result = Number(result);
-
-    // result = Number(result);
-    return result
-}
-
-setTimeout(()=>{
-
-    console.log(arg([2,4,2,2,4,5,4]));
-
-},3000)
+function sum(a , b ) {
+    return a + b;
+};
 
 
+let xyz = function (a , b ) {
+    return a + b;
+};
 
